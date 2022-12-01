26fb0eb7636f29d07d31b5892f656885d61de6ac
Nehanegi97 <nehanegi071097@gmail.com>
2022-12-01 08:50:42 +0530

    server

--- src/Providers/NotesProviders.js
+++ src/Providers/NotesProviders.js
@@ -1,45 +1,39 @@
 import React, { useState } from "react";
 import uuid from 'uuid';
+import axios from "axios";
 
 export const Context = React.createContext();
 
 const Provider = props => {
-    const [data, setData] = useState([{
-        id: "1",
-        title: "first note here 1",
-        detail: "complete first task"
-    },
-    {
-        id: "2",
-        title: "first note here 2",
-        detail: "complete first task"
-    },
-    {
-        id: "3",
-        title: "first note here 3",
-        detail: "complete first task"
-    },
-    {
-        id: "4",
-        title: "first note here 4",
-        detail: "complete first task"
-    }])
+    const [data, setData] = useState()
+
+    React.useEffect(() => {
+        const loadNotes = async () => {
+            try {
+                const response = await axios.get("/notes");
+                setData(response.data);
+            } catch (e) {
+                console.log("rejected", e);
+            }
+        };
+        loadNotes();
+    }, []);
 
     const deleteNote = (e, id) => {
         const newNotes = data.filter((note) => note.id !== id)
         setData(newNotes)
     }
 
-    const addNote = (e,title,detail) => {
+    const addNote = (e, title, detail) => {
         setData((prev) => [
-            ...prev,{
-                id:uuid(),
-                title:title,
-                detail:detail
-            }    
+            ...prev, {
+                id: uuid(),
+                title: title,
+                detail: detail
+            }
         ])
     }
-    
+
     return (
         <Context.Provider
             value={{
--- src/components/AddNoteForm/index.jsx
+++ src/components/AddNoteForm/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 
-
 function AddNoteForm({title,setTitle,detail,setDetail}) {
     return (
         <>
